cc11531ef1e11363a629074bdcd3f31b627e54c2
wangsiyuan <wangsiyuan13@meituan.com>
2020-06-14 17:07:14 +0800

    master

--- src/main/java/com/wsy/learn/lucene/GoodsIndex.java
+++ src/main/java/com/wsy/learn/lucene/GoodsIndex.java
@@ -9,6 +9,7 @@ import org.apache.lucene.analysis.cn.smart.SmartChineseAnalyzer;
 import org.apache.lucene.document.*;
 import org.apache.lucene.index.IndexWriter;
 import org.apache.lucene.index.IndexWriterConfig;
+import org.apache.lucene.index.IndexableField;
 import org.apache.lucene.store.Directory;
 import org.apache.lucene.store.FSDirectory;
 
@@ -19,26 +20,29 @@ public class GoodsIndex {
     public static void main(String[] args) {
 
         try {
-            String indexPath = "/Users/wangsiyuan1/workspace/learnlucene/src/main/resources/goods";
+            String home = "/Users/wangsiyuan/source/learn-lucene/";
+            String indexPath = home + "src/main/resources/goods";
             Analyzer analyzer = new SmartChineseAnalyzer();
             IndexWriterConfig iwc = new IndexWriterConfig(analyzer);
             Directory dir = FSDirectory.open(Paths.get(indexPath));
             IndexWriter indexWriter = new IndexWriter(dir, iwc);
 
-            String jsonStr = FileUtils.readFileToString(new File("/Users/wangsiyuan1/workspace/learnlucene/src/main/resources/doc/goods.json"));
+            String jsonStr = FileUtils.readFileToString(new File(home + "src/main/resources/doc/goods.json"));
             JSONArray array = JSON.parseArray(jsonStr);
             for (int i = 0, length = array.size(); i < length; i++) {
                 JSONObject json = array.getJSONObject(i);
 
                 Document doc = new Document();
-                doc.add(new TextField("title", json.getString("title"), Field.Store.YES));
+                doc.add(new TextField("title", json.getString("title"), Field.Store.NO));
                 doc.add(new LongPoint("skuId", json.getLong("skuId")));
+                doc.add(new NumericDocValuesField("skuId", json.getLong("skuId")));
+                doc.add(new StoredField("skuId", json.getLong("skuId")));
                 doc.add(new IntPoint("cid1", json.getInteger("cid1")));
-                doc.add(new StringField("cid1Name", json.getString("cid1Name"), Field.Store.YES));
+                doc.add(new StringField("cid1Name", json.getString("cid1Name"), Field.Store.NO));
                 doc.add(new IntPoint("cid2", json.getInteger("cid2")));
-                doc.add(new StringField("cid2Name", json.getString("cid2Name"), Field.Store.YES));
+                doc.add(new StringField("cid2Name", json.getString("cid2Name"), Field.Store.NO));
                 doc.add(new IntPoint("cid3", json.getInteger("cid3")));
-                doc.add(new StringField("cid3Name", json.getString("cid3Name"), Field.Store.YES));
+                doc.add(new StringField("cid3Name", json.getString("cid3Name"), Field.Store.NO));
                 doc.add(new DoublePoint("price", json.getDoubleValue("price")));
                 doc.add(new LongPoint("createTime", json.getLongValue("createTime")));
                 indexWriter.addDocument(doc);
@@ -46,7 +50,7 @@ public class GoodsIndex {
             indexWriter.close();
 
         } catch (Exception e) {
-
+            e.printStackTrace();
         }
     }
 }
--- src/main/java/com/wsy/learn/lucene/GoodsSearch.java
+++ src/main/java/com/wsy/learn/lucene/GoodsSearch.java
@@ -1,5 +1,6 @@
 package com.wsy.learn.lucene;
 
+import org.apache.lucene.document.Document;
 import org.apache.lucene.index.DirectoryReader;
 import org.apache.lucene.index.IndexReader;
 import org.apache.lucene.index.Term;
@@ -12,12 +13,13 @@ import java.nio.file.Paths;
 public class GoodsSearch {
     public static void main(String[] args) {
         try {
-            String indexPath = "/Users/wangsiyuan1/workspace/learnlucene/src/main/resources/goods";
+            String home = "/Users/wangsiyuan/source/learn-lucene/";
+            String indexPath = home + "src/main/resources/goods";
             IndexReader reader = DirectoryReader.open(FSDirectory.open(Paths.get(indexPath)));
             IndexSearcher searcher = new IndexSearcher(reader);
-
             for (ScoreDoc doc : query(searcher)) {
-                System.out.println(doc.toString());
+                Document document = searcher.doc(doc.doc);
+                System.out.println(document.getField("skuId").numericValue().longValue());
             }
         } catch (Exception e) {
             e.printStackTrace();
@@ -25,13 +27,14 @@ public class GoodsSearch {
     }
 
     public static ScoreDoc[] query(IndexSearcher searcher) throws IOException {
-        Query query1 = new TermQuery(new Term("title", "男装"));
-        Query query2 = new TermQuery(new Term("cid1", "13212"));
+        Query query1 = new TermQuery(new Term("title", "南极"));
+        Query query2 = new TermQuery(new Term("cid1", "13152"));
         BooleanClause bc1 = new BooleanClause(query1, BooleanClause.Occur.MUST);
         BooleanClause bc2 = new BooleanClause(query2, BooleanClause.Occur.SHOULD);
         BooleanQuery boolQuery = new BooleanQuery.Builder().add(bc1).add(bc2).build();
         // 返回前10条
         TopDocs topDocs = searcher.search(boolQuery, 10);
+        System.out.println("hit num:" + topDocs.totalHits);
         return topDocs.scoreDocs;
     }
 }
